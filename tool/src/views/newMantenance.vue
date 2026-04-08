4fdd08c1ea83e2a2c461bfe3b75b5f95781fdfe6
Franco Acevedo <francoeacevedo37@gmail.com>
2026-04-08 10:58:39 -0300

    fix de cargar horometro como operario

--- tool/src/views/newMantenance.vue
+++ tool/src/views/newMantenance.vue
@@ -644,6 +644,22 @@ this.showMachineDetailModal = false
 
 },
 
+formatDate(value) {
+const date = new Date(value)
+if (Number.isNaN(date.valueOf())) {
+return "-"
+}
+
+return date.toLocaleString("es-AR", {
+year: "numeric",
+month: "2-digit",
+day: "2-digit",
+hour: "2-digit",
+minute: "2-digit",
+second: "2-digit"
+})
+},
+
 }
 
 }
